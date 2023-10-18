6003bf0acb362ac6e338a4bbe92f300cb504d129
David-Yuan1107 <kewei.yuan@student.unimelb.edu.au>
2023-10-18 15:33:56 +1100

    Revised to use random primes for brute force RSA

--- bruteforce.py
+++ bruteforce.py
@@ -10,6 +10,9 @@ from string import ascii_letters, digits
 import utils
 from RSA import RSA
 
+MINBIT = 2
+MAXBIT = 30
+
 # function to factorize an integer n
 def fac(n):
     max = math.ceil(math.sqrt(n))
@@ -29,18 +32,14 @@ def main():
     time_data = {}
     
     # Looping through different bit lengths
-    for n_bits in range(30, 33):  # Example bit lengths 30, 31, 32
-        # Start time
-        start_time = time.time()
-        
+    for n_bits in range(MINBIT, MAXBIT):
+
         # Generate n-bit prime numbers for p and q
         p = utils.randPrime(n_bits=n_bits)
         q = utils.randPrime(n_bits=n_bits)
         
-        # Existing RSA and brute-force logic (To be filled in)
-        # ...
-        
-        # End time
+        start_time = time.time()
+        p= fac(p*q)
         end_time = time.time()
         
         # Calculate time taken
@@ -50,45 +49,21 @@ def main():
         time_data[n_bits] = time_taken
         
         print(f"Time taken for {n_bits}-bit RSA: {time_taken} seconds")
-    
-def main():
-    #p = random.choice()
-    p = 66424187
-    q = 87006341
-    #rsa = RSA(p,q)
-    count = 10
-    m = "random"
-    time = []
-    length = []
-    #
-    pqdict = [(3,5),(17,11),(191,173),(45659,59497),(236773,283741),(1024391,1003003),(13390961,10087351),(182468119,164535253)]
-    #c = rsa.encrypt(m)
-    for (p,q)in pqdict:
-        start = datetime.datetime.now()
-        p= fac(p*q)
 
-        end = datetime.datetime.now()
-
-        time.append((end-start).total_seconds())
-        length.append(p.bit_length())
-    print(time)
     plt.axis('auto')
 
-    for i in range(len(length)):
-        plt.annotate(str(length[i]),
-    xy=(length[i], time[i]), xycoords='data',
+    for i in range(MINBIT, MAXBIT):
+        plt.annotate(str(i),
+    xy=(i, time_data[i]), xycoords='data',
     xytext=(5, 0), textcoords='offset points',
     size=7,
     bbox=dict(boxstyle="round4,pad=.5", fc="0.8", alpha=0.4),
     arrowprops=dict(arrowstyle="-"))
-    plt.plot(length,time)
+    plt.plot(time_data.keys(),time_data.values())
     plt.xlabel("Length(bits)")
     plt.ylabel("Time")
     plt.show()
     return 1
-    
-
-
 
 if __name__ == "__main__":
     main()
